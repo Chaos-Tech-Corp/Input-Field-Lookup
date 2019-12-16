6f2c4a86b56f3992c3efb482795f84b323154604
Javier Chaos <jachaos@gmail.com>
2019-12-15 23:00:13 -0500

    Update lookupField.js

--- src/lookupField.js
+++ src/lookupField.js
@@ -1,193 +1,88 @@
 ({
-    doInit : function(component,event,helper){
-        var selectedId = component.get("v.selectedId");
-        var action = component.get("c.getObjectDetails");
-        action.setParams({'ObjectName' : component.get("v.objectAPIName")});
-        action.setCallback(this, function(response) {
-            var details = response.getReturnValue();
-            component.set("v.IconName", details.iconName);
-            component.set("v.objectLabel", details.label);
-            component.set("v.objectLabelPlural", details.pluralLabel);
-            if (selectedId == null || selectedId.trim().length <= 0) {
-            	component.set("v.isLoading", false);
-            }
-        });
-        $A.enqueueAction(action);
+    doInit : function(component, event, helper) {
+		//show spinner
+        component.set("v.Loading", true);
         
-        var returnFields =  component.get("v.returnFields"),
-            queryFields =  component.get("v.queryFields");
-        
-        if (returnFields == null || returnFields.length <= 0) {
-            component.set("v.returnFields", ['Name']);
-        }
-        
-        if (queryFields == null || queryFields.length <= 0) {
-            component.set("v.queryFields", ['Name']);
-        }
-        
-        //help for cancelling the create new record
-        //find the latest accessed record for the user
-        if (component.get("v.showAddNew")) {
-            var action = component.get("c.GetRecentRecords");
-            action.setParams({
-                'ObjectName' : component.get("v.objectAPIName"),
-                'ReturnFields' :  null,
-                'MaxResults' : 1
-            });
-            action.setCallback(this, function(response) {
-                var results = response.getReturnValue();
-                if (results != null && results.length > 0) {
-					component.lastRecordId = results[0].Id;
+        component.set('v.tableColumns', [
+            {label: 'Product', fieldName: 'Product2Name', type: 'text'},
+            {label: 'Quantity', fieldName: 'Quantity', type: 'number'},
+            {label: 'Total Price', fieldName: 'TotalPrice', type: 'currency'},
+            {
+                label: 'Start date',
+                fieldName: 'ServiceDate',
+                type: 'date',
+                typeAttributes: {
+                    year: 'numeric',
+                    month: 'short',
+                    day: 'numeric'
                 }
-            });
-            $A.enqueueAction(action);
-        }
-        if (selectedId != null && selectedId.trim().length > 0) {
-            var action = component.get("c.GetRecord"),
-                returnFields = component.get("v.returnFields");
-            action.setParams({'ObjectName' : component.get("v.objectAPIName"),
-                              'ReturnFields': returnFields,
-                              'Id': component.get("v.selectedId")});
-            action.setCallback(this, function(response) {
-                var results = response.getReturnValue();
-                results = helper.processResults(results, returnFields);
-                component.set("v.selectedName", results[0].Field0);
-                component.set("v.isLoading", false);
-            });
-            $A.enqueueAction(action);
-        }
-    },
-    
-    onFocus : function(component,event,helper){
-        var inputBox = component.find("lookup-input-box"),
-            searchText = component.get("v.searchText") || '';
+            },
+            {label: 'Fund', fieldName: 'FundName', type: 'text'}
+        ]);
         
-        $A.util.addClass(inputBox, 'slds-is-open');
-        $A.util.removeClass(inputBox, 'slds-is-close');
-        
-        if (component.get("v.showRecent") && searchText.trim() == '') {
-            component.set("v.isSearching", true);        
-            var action = component.get("c.GetRecentRecords"),
-                returnFields = component.get("v.returnFields");
+        let action = component.get("c.GetSiblings");
+        action.setParams({ Id : component.get("v.recordId") });
+        action.setCallback(this, function(response) {
+            component.set("v.relatedOrders", response.getReturnValue());
+            //hide spinner
+        	component.set("v.Loading", false);
             
-            action.setParams({
-                'ObjectName' : component.get("v.objectAPIName"),
-                'ReturnFields' :  returnFields,
-                'MaxResults' : component.get("v.maxResults")
-            });
-            action.setCallback(this, function(response) {
-                var results = response.getReturnValue();
-                if (results != null) {
-                    component.set("v.statusMessage", results.length > 0 ? null : 'No recent records.' );
-                    component.set("v.searchResult", 
-                                  helper.processResults(results, returnFields));
-                } else {
-                    component.set("v.statusMessage", "Search Error!" );
-                }
-                component.set("v.isSearching", false);
-            });
-            $A.enqueueAction(action);
-        }
-        
-    },
-    
-    onBlur : function(component,event,helper){       
-        var inputBox = component.find("lookup-input-box");
-        $A.util.addClass(inputBox, 'slds-is-close');
-        $A.util.removeClass(inputBox, 'slds-is-open');
-        
-        $A.util.removeClass(component.find("lookup-input-box"),'slds-has-focus');
-        
-    },
-    
-    onKeyUp : function(component, event, helper) {
-        
-        var searchText = component.get('v.searchText');
-        //do not repeat the search if nothing changed
-        if (component.lastSearchText !== searchText) {
-            component.lastSearchText = searchText;
-        } else {
-            return;
-        }
-        
-        if (searchText == null || searchText.trim().length < 3) {
-            component.set("v.searchResult", []);
-            component.set("v.statusMessage", null);
-            return;
-        }
-        
-        component.set("v.isSearching", true);        
-        var action = component.get("c.SearchRecords"),
-            returnFields = component.get("v.returnFields");
-        
-        action.setParams({
-            'ObjectName' : component.get("v.objectAPIName"),
-            'ReturnFields' :  returnFields,
-            'QueryFields' :  component.get("v.queryFields"),
-            'SearchText': searchText,
-            'SortColumn' : component.get("v.sortColumn"),
-            'SortOrder' : component.get("v.sortOrder"),
-            'MaxResults' : component.get("v.maxResults")
         });
+        $A.enqueueAction(action);
         
+        
+	},
+    
+
+    handleRefreshClick : function(component, event, helper) {
+        $A.get('e.force:refreshView').fire();
+    },
+    
+    showSplit : function(component, event, helper) {
+        component.set("v.Loading", true);
+        let action = component.get("c.GetOrderItems");
+        action.setParams({ Id : component.get("v.recordId") });
         action.setCallback(this, function(response) {
-            var results = response.getReturnValue();
-            if (results != null) {
-                component.set("v.statusMessage", results.length > 0 ? null : 'No records found.' );
-                component.set("v.searchResult", 
-                              helper.processResults(results, returnFields));
-            } else {
-                component.set("v.statusMessage", results.length > 0 ? null : 'Search Error!' );
+            var data = response.getReturnValue();
+            for(var i = 0; i < data.length; i++) {
+                data[i].Product2Name = data[i].Product2.Name;
+                if (data[i].Order_Fund__r) {
+                data[i].FundName = data[i].Order_Fund__r.Fund_F_Name__c;
+                }
             }
-            component.set("v.isSearching", false);
+            component.set("v.data", data);
+            component.set("v.showSplit", true);
+            component.set("v.Loading", false);
         });
         $A.enqueueAction(action);
         
     },
     
-    onSelectItem : function(component, event, helper) {
-        var selectedId = event.currentTarget.dataset.id;
-        component.set("v.selectedId", selectedId);
-        var results = component.get("v.searchResult");
-        for (var i = 0; i < results.length; i++) {
-            if (results[i].Id == selectedId) {
-                component.set("v.selectedName", results[i].Field0);
-                break;
-            }
+    hideSplit : function(component, event, helper) {
+        component.set("v.showSplit", false);
+    },
+    
+    updateSelectedRows : function(component, event, helper) {
+        var selectedRows = event.getParam('selectedRows');
+        component.set('v.selectedRows', selectedRows);
+    },
+    
+    doSplit : function(component, event, helper) {
+        component.set("v.Loading", true);
+        var selectedRows = component.get('v.selectedRows'),
+            selectedIds = [];
+        
+        for (var i = 0; i< selectedRows.length; i++) {
+            selectedIds.push(selectedRows[i].Id);
         }
-        $A.enqueueAction(component.get("c.onBlur"));
-    },
-    
-    removeSelectedOption : function(component, event, helper) {
-        component.set("v.selectedId", null);
-    },
-    
-    createNewRecord : function(component, event, helper) {
-        var createRecordEvent = $A.get("e.force:createRecord"),
-            objectName = component.get("v.objectAPIName"),
-            returnFields = component.get("v.returnFields");
-        createRecordEvent.setParams({
-            "entityApiName": objectName,
-            "navigationLocation" : "LOOKUP",
-            "panelOnDestroyCallback": function(event) {
-                let action = component.get("c.GetRecentRecords");
-                action.setParams({'ObjectName' : objectName,
-                                  'MaxResults' : 1,
-                                  'ReturnFields': returnFields});
-                action.setCallback(this, function(response) {
-                    var records = response.getReturnValue();
-                    if (records != null && records.length > 0) {
-                        if (records[0].Id != component.lastRecordId) {
-                            component.set("v.selectedId", records[0].Id);
-                            component.set("v.selectedName", records[0][returnFields[0]]);
-                            component.lastRecordId = records[0].Id;
-                        }
-                    }
-                });
-                $A.enqueueAction(action);              
-            }
+        console.log(selectedIds);
+        component.set("v.showSplit", false);
+        let action = component.get("c.SplitOrder");
+        action.setParams({ Id : component.get("v.recordId"), ProductIds: selectedIds });
+        action.setCallback(this, function(response) {
+            $A.get('e.force:refreshView').fire();
         });
-        createRecordEvent.fire();
-    }
+        $A.enqueueAction(action);
 
+    }
 })
